7e041868e1f758391b4849aa3d4aaf29f1ce6ce1
Justin Chan <70335804+sadminis@users.noreply.github.com>
2023-12-29 16:58:08 -0700

    Update main.go
    Created menu

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hello World")
+func main() {
+	var menu string
+	var input string
+	menu = "Enter your option: \n1. Store Username/Password\n2. Search Username/Password"
+
+	fmt.Println(menu)
+	fmt.Scanln(&input)
+
+	switch input {
+	case "1":
+		fmt.Print("Entered 1 \n")
+	case "2":
+		fmt.Print("Entered 2 \n")
+	default:
+		fmt.Print("Invalid input \n")
+	}
 }
